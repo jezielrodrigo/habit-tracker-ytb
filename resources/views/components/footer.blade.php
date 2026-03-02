f802ad1f547b6793de432a46ac88a9f29285cc5f
jezielrodrigo <jeziel@grandebauru.com.br>
2026-03-02 14:21:33 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+    <p class="text-center"> 
+        Criado por Jeziel Rodrigo. O codigo fonte esta no <a href="#" class="underline"> GitHub </a>    
+    </p>
+</footer>
