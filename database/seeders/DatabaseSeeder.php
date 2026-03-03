b7af1fc60506ab7bded6e2e35798664ac06be0cf
jezielrodrigo <jeziel@grandebauru.com.br>
2026-03-03 16:42:12 -0300

    feat(Habit/HabitLog): create factories/seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Habit;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,8 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(
-                UserSeeder::class
-        );
+        $this->call([
+            UserSeeder::class,
+            HabitSeeder::class,
+            HabitLogSeeder::class,
+        ]);
     }
 }
